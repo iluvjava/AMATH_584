c1c5c63f322ca89a60babec4a7841a24e8469eec
Alto Legato <victor1301166040@gmail.com>
2020-10-27 10:47:56 -0700

    d

--- HW3/TestModifiedGSBasic.m
+++ HW3/TestModifiedGSBasic.m
@@ -5,6 +5,7 @@ Trials = 10; M = 1000, N = 500;
 ReconstructionError = zeros(1, Trials);
 IdentityError = zeros(1, Trials);
 IdentityMatrix = eye(N);
+
 for Trial = 1: Trials
    RandMatrix = rand(M, N);
    [Q, R] = ModifiedGS(RandMatrix);
